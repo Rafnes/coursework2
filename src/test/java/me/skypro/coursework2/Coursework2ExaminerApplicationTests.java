6b24d336f668405865f786c88fd77d5a2e8c7694
Rafnes <rafzograf@gmail.com>
2024-10-02 15:09:01 +0300

    initial commit

---
+++ src/test/java/me/skypro/coursework2/Coursework2ExaminerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.skypro.coursework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework2ExaminerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
